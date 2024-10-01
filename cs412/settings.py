efd14506669154c40352d48e1467589070a28d1d
Victoria Lin <vicki@bu.edu>
2024-09-30 20:39:22 -0400

    fixing css

--- cs412/settings.py
+++ cs412/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
